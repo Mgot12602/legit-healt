19151aba6a7880f166fdfc804178a58b34ce6bc7
Mgot12602 <63016167+Mgot12602@users.noreply.github.com>
2024-04-07 23:41:23 +0200

    feat: Improved Error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import { Button, Container, Typography } from "@mui/material";
+import {Link} from 'react-router-dom'
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <Container sx={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      textAlign: 'center', 
+      p: 2 
+    }}>
+      <Typography variant="h1">Oops!</Typography>
+      <Typography variant="h4">Something went wrong.</Typography>
+      <Button variant="contained" component={Link} to={'/'} color="primary" sx={{ mt: 2 }}>
+        Go Back
+      </Button>
+    </Container>
+  );
+};
 
 export default ErrorPage
