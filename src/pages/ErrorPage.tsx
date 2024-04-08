9dc897d600a61449814b9756a3bf4abf2d083de7
Mgot12602 <63016167+Mgot12602@users.noreply.github.com>
2024-04-08 07:59:56 +0200

    fix: used plain html and css for the Error Page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,23 +1,32 @@
-import { Button, Container, Typography } from "@mui/material";
-import {Link} from 'react-router-dom'
-const ErrorPage = () => {
+function ErrorPage() {
   return (
-    <Container sx={{ 
+    <div style={{ 
       display: 'flex', 
       flexDirection: 'column', 
       justifyContent: 'center', 
       alignItems: 'center', 
       height: '100vh', 
       textAlign: 'center', 
-      p: 2 
+      padding: '16px' 
     }}>
-      <Typography variant="h1">Oops!</Typography>
-      <Typography variant="h4">Something went wrong.</Typography>
-      <Button variant="contained" component={Link} to={'/'} color="primary" sx={{ mt: 2 }}>
+      <h1 style={{ fontSize: '2em', margin: '0.67em 0' }}>Oops!</h1>
+      <h4 style={{ fontSize: '1.33em', margin: '0.67em 0' }}>Something went wrong.</h4>
+      <a href="/" style={{
+        display: 'inline-block',
+        padding: '6px 16px',
+        fontSize: '0.875rem',
+        lineHeight: '1.75',
+        color: '#fff',
+        backgroundColor: '#3f51b5',
+        borderRadius: '4px',
+        textDecoration: 'none',
+        marginTop: '16px',
+        textAlign: 'center'
+      }}>
         Go Back
-      </Button>
-    </Container>
+      </a>
+    </div>
   );
 };
 
-export default ErrorPage
+export default ErrorPage;
